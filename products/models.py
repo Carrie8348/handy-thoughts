df4c11d4db7ebf3084d5d517aa12d11d428a4b48
Carrie8348 <maruko_carrie@hotmail.com>
2022-07-24 09:48:41 +0000

    Added product detail functionality

--- products/models.py
+++ products/models.py
@@ -25,9 +25,12 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    brand = models.CharField(max_length=100, null=True, blank=True)
     description = models.TextField()
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+    
