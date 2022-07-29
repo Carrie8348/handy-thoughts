094d11ef5cb2732d04ee532a82396e16a16b44df
Carrie8348 <maruko_carrie@hotmail.com>
2022-07-29 09:41:55 +0000

    Added edit and remove views for shopping bag, and added related pages to link with shopping bag templates

--- shopping_cart/urls.py
+++ shopping_cart/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.view_shopping_cart, name="view_shopping_cart"),
     path('add/<item_id>/', views.add_to_shopping_cart, name="add_to_shopping_cart"),
+    path('adjust/<item_id>/', views.adjust_shopping_cart, name='adjust_shopping_cart'),
+    path('remove/<item_id>/', views.remove_from_shopping_cart, name='remove_from_shopping_cart'),
 ]
--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect,get_object_or_404
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 from products.models import Product
 
@@ -26,3 +26,37 @@ def add_to_shopping_cart(request, item_id):
 
     request.session['shopping_cart'] = shopping_cart
     return redirect(redirect_url)
+
+def adjust_shopping_cart(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    shopping_cart = request.session.get('shopping_cart', {})
+
+ 
+    if quantity > 0:
+        shopping_cart[item_id] = quantity
+        messages.success(request, f'Updated {product.name} quantity to {shopping_cart[item_id]}')
+    else:
+        shopping_cart.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your shopping cart')
+
+    request.session['shopping_cart'] = shopping_cart
+    return redirect(reverse('view_shopping_cart'))
+
+def remove_from_shopping_cart(request, item_id):
+    """Remove the item from the shopping cart"""
+
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+        shopping_cart = request.session.get('shopping_cart', {})
+        shopping_cart.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your shopping cart')
+
+        request.session['shopping_cart'] = shopping_cart
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
