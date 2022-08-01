00374cd2216c3d9def87b863a1a59e01edd25b5f
Carrie8348 <maruko_carrie@hotmail.com>
2022-08-01 12:17:54 +0000

    Finished checkout templates and urls

--- handy_thoughts/settings.py
+++ handy_thoughts/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
     'products',
     'shopping_cart',
     'checkout',
+
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +62,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'handy_thoughts.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -75,8 +80,13 @@ TEMPLATES = [
                 #required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'shopping_cart.contexts.shopping_cart_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
