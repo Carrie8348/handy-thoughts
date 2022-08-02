a7344677ea580822ad10307149488243002be5c0
Carrie8348 <maruko_carrie@hotmail.com>
2022-08-02 07:29:32 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HUQulGSUzkapEeIAnA4WnNlKPWynJaQCoyNPip6JuGD8S898ylIjwWmuQixvXltHCCS8NpeH5KeoiAiOgSP1CUo00OIWOCf8t',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
