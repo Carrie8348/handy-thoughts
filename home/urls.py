e3ba8119678a520d067b3139a185fa9d4db7711e
Carrie8348 <maruko_carrie@hotmail.com>
2022-08-21 09:06:08 +0000

    fixed home view prob

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,5 @@ from .import views
 urlpatterns = [
     path('', views.index, name="home"),
     path('shipping_policy/', views.shipping, name="shipping"),
-    path('about_us/', views.about_us(request), name="about_us"),
+    path('about_us/', views.about_us, name="about_us"),
 ]
