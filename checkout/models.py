35b27e1e163c1be327a56fe11260e779781b7d99
Carrie8348 <maruko_carrie@hotmail.com>
2022-08-22 16:09:54 +0000

    error fix

--- checkout/models.py
+++ checkout/models.py
@@ -13,8 +13,8 @@ from profiles.models import UserProfile
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
-    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, 
-                                     null=True, blank=True, 
+    user_profile = models.ForeignKey(UserProfile,on_delete=models.SET_NULL, 
+                                     null=True, blank=True,
                                      related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
@@ -32,7 +32,7 @@ class Order(models.Model):
                                       null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2,
                                       null=False, default=0)
-    original_shopping_cart = models.TextField(null=False, blank=False,
+    original_shopping_cart = models.TextField(null=False, blank=False, 
                                               default='')
     stripe_pid = models.CharField(max_length=254, null=False, blank=False,
                                   default='')
@@ -48,10 +48,9 @@ class Order(models.Model):
         Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate (Sum('lineitem_total'))['lineitem_total__sum'] or 0
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
-            self.delivery_cost =
-            self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
             self.delivery_cost = 0
         self.grand_total = self.order_total + self.delivery_cost
